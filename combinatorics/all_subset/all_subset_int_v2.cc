e0319e4822a6683bc6d8f717ec9e9458ccad81f9
diptamath <dipta.math@gmail.com>
2018-04-03 09:26:52 +0530

    library for online competitive coding contest (Updating)
    A c++ library/implementation/collection on data structures & algorithms
    for online competitive coding contest for personal uses.

---
+++ combinatorics/all_subset/all_subset_int_v2.cc
@@ -0,0 +1,30 @@
+/*
+  All subset (int) (version 2).
+
+  Constructing all subsets of 1, 2, ..., n via backtracking.
+
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void all_subsets(string& S, int l) {
+  if (l == 0) {
+    cout << S << endl;
+    return;
+  }
+
+  for (int i = 0; i < 2; i++) {
+    S[l] = i + '0';
+    all_subsets(S, l-1);
+  }
+}
+
+int main() {
+  int l;
+  cin >> l;
+  string tmp("");
+  tmp.resize(l+1);
+  all_subsets(tmp, l);
+}
